4b2b7d1e19603f38098650c49eb856ae332fb631
Bhagyansh Sahu <ashusahu2k@gmail.com>
2025-05-28 22:59:29 +0530

    ppm file made using g++ compiler

---
+++ week 1/build/CMakeFiles/first_render.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first_render.dir/main.cpp.obj"
+  "CMakeFiles/first_render.dir/main.cpp.obj.d"
+  "first_render.exe"
+  "first_render.exe.manifest"
+  "first_render.pdb"
+  "libfirst_render.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first_render.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week 1/build/CMakeFiles/first_render.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for first_render.
+# This may be replaced when dependencies are built.
