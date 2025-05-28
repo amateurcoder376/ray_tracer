d364cae4ab73435c9f6a2875aec1ed2ff62c2dd1
Bhagyansh Sahu <ashusahu2k@gmail.com>
2025-05-28 22:48:47 +0530

    :]

--- week 1/main.cpp
+++ week 1/main.cpp
@@ -1,34 +1,29 @@
-#include "vec3.h"
 #include<iostream>
 
 using namespace std;
 
 int main(){
-    float x,y,z;
-    cout<<"Enter vector A (x y z): ";
-    cin >> x >> y >> z;
-    Vec3 A(x,y,z);
-    cout<<"Enter vector B (x y z): ";
-    cin >> x >> y >> z;
-    Vec3 B(x,y,z);
-    cout<<"Enter vector N (x y z): ";
-    cin >> x >> y >> z;
-    Vec3 N(x,y,z);
-    cout<<"Enter refractive index (e.g., 4/3 = 1.333): "; 
-    float refractive;
-    cin>>refractive;
-    cout<<endl;cout<<endl;
-    cout<<"Vector A: "<<A[0]<<" "<<A[1]<<" "<<A[2]<<" "<<endl;
-    cout<<"Vector B: "<<B[0]<<" "<<B[1]<<" "<<B[2]<<" "<<endl;
-    cout<<"Vector N: "<<N[0]<<" "<<N[1]<<" "<<N[2]<<" "<<endl;
-    cout<<"Refractive Index (eta): "<<refractive<<endl;
-    cout<<endl;
-    cout<<"A + B = "<<(A+B)[0]<<" "<<(A+B)[1]<<" "<<(A+B)[2]<<" "<<endl;
-    cout<<"A - B = "<<(A-B)[0]<<" "<<(A-B)[1]<<" "<<(A-B)[2]<<" "<<endl;
-    cout<<"A dot B = "<<A.dot(B)<<endl;
-    cout<<"A cross B = "<<(A.cross(B))[0]<<" "<<(A.cross(B))[1]<<" "<<(A.cross(B))[2]<<" "<<endl;
-    cout<<"Length of A = "<<A.length()<<endl;
-    cout<<"Unit vector of A = "<<(A.unit())[0]<<" "<<(A.unit())[1]<<" "<<(A.unit())[2]<<endl;
-    cout<<"Reflect A about N = "<<(A.reflect(N))[0]<<" "<<(A.reflect(N))[1]<<" "<<(A.reflect(N))[2]<<endl;
-    cout<<"Refract A through N = "<<(A.refract(N,refractive))[0]<<" "<<(A.refract(N,refractive))[1]<<" "<<(A.refract(N,refractive))[2]<<endl;
+    int image_width=256,image_height=256;
+
+    cout<<"P3"<<endl; //it tells its in ascii value
+    cout<<"256"<<" "<<"256"<<endl; //rows and coloumn
+    cout<<"255"<<endl;//max colour
+
+    //RGB triplets
+
+    for (int j = 0; j < image_height; j++) {
+        clog << "\rScanlines remaining: " << (image_height - j) << ' ' << endl << flush;
+        for (int i = 0; i < image_width; i++) {
+            auto r = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+    clog << "\rDone.                 \n";
 }
